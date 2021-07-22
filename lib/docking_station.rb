8c30aab3c66694ce22a0fb4777b7e239dd191aa0
Dylan-P-H <Dylan@eequ.org>
2021-07-22 19:06:18 +0100

    2nd finish of question 11

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,8 +1,12 @@
 require "bike"
 
 class DockingStation
+  attr_reader :bike
   def release_bike
     Bike.new
+  end
+  def dock(bike)
+    @bike = bike
   end
 end
 
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,10 +2,24 @@ require "docking_station"
 require "bike"
 
 describe DockingStation do
+
   it { is_expected.to respond_to :release_bike }
 
   it 'releases working bikes' do
     bike = subject.release_bike
     expect(bike).to be_working
+  end
+
+  it 'docks something' do
+    bike = Bike.new
+    # We want to return the bike we dock
+    expect(subject.dock(bike)).to eq bike
+  end
+
+  it 'returns docked bikes' do
+    bike = Bike.new
+    subject.dock(bike)
+    # Again, we need to return the bike we just docked
+    expect(subject.bike).to eq bike
   end
 end
