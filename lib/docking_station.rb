f057bd96f7a7bcfec31c8369bfde5bcb7e36e762
Dylan-P-H <Dylan@eequ.org>
2021-07-22 20:12:44 +0100

    completed question 15 but working on a few errors

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,12 +1,31 @@
 require "bike"
 
 class DockingStation
+
   attr_reader :bike
-  def release_bike
-    Bike.new
+
+  def initialize
+    @bikes = []
   end
+
+  def release_bike
+    fail 'No bikes available' if empty?
+    @bikes.pop
+  end
+
   def dock(bike)
-    @bike = bike
+    fail 'Docking station full' if full?
+    @bikes << bike
+  end
+
+  private
+
+  def full?
+    @bikes.count >= 20
+  end
+
+  def empty?
+    @bikes.empty?
   end
 end
 
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,7 +3,19 @@ require "bike"
 
 describe DockingStation do
 
-  it { is_expected.to respond_to :release_bike }
+  describe '#release_bike' do
+    it 'releases a bike' do
+      bike = Bike.new
+      subject.dock(bike)
+      expect(subject.release_bike).to eq bike
+    end
+  end
+
+  describe '#release_bike' do
+    it 'raises an error when there are no bikes available' do
+      expect { subject.release_bike }.to raise_error 'No bikes available'
+    end
+  end
 
   it 'releases working bikes' do
     bike = subject.release_bike
@@ -12,14 +24,19 @@ describe DockingStation do
 
   it 'docks something' do
     bike = Bike.new
-    # We want to return the bike we dock
     expect(subject.dock(bike)).to eq bike
   end
 
   it 'returns docked bikes' do
     bike = Bike.new
     subject.dock(bike)
-    # Again, we need to return the bike we just docked
     expect(subject.bike).to eq bike
+  end
+  
+  describe '#dock' do
+    it 'raises an error when full' do
+      20.times { subject.dock Bike.new }
+      expect { subject.dock Bike.new }.to raise_error 'Docking station full'
+    end
   end
 end
